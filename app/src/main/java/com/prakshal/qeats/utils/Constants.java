263cd4985ee8fb0a939498ac96e0f4160b5c0ed7
atishaya11 <atishaya.11@live.com>
2019-06-18 00:09:11 +0530

    Added constants

---
+++ app/src/main/java/com/prakshal/qeats/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.prakshal.qeats.utils;
+
+public class Constants {
+
+    public static String BASE_URL = "http://35.200.159.249:8081";
+
+    public static final String API_ENDPOINT = BASE_URL + "/qeats/v1";
+
+    public static final String RESTAURANTS_API = "/restaurants";
+
+    public static final String MENU_API = "/menu";
+    public static final String CART_API = "/cart";
+    public static final String CART_ITEM_API = "/cart/item";
+    public static final String CART_CLEAR_API = "/cart/clear";
+    public static final String POST_ORDER_API = "/order";
+    public static final String GET_ORDER_API = "/order";
+    public static final String GET_ORDERS_API = "/orders";
+
+
+}
